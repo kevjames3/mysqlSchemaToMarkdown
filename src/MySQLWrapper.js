ea635022717edd571fbce5668d36885abfbe8566
Kevin McIntosh <kevjames3@gmail.com>
2017-11-26 14:06:05 -0800

    Added basic wrapper that consolidates a lot of the functionality

---
+++ src/MySQLWrapper.js
@@ -0,0 +1,42 @@
+'use strict';
+let mysql       = require('mysql');
+let connection  = undefined;
+let isConnectionOpen = false;
+
+module.exports = class MySQLWrapper {
+  constructor(dbConfig){
+    this.dbConfig = dbConfig;
+
+    connection = mysql.createConnection({
+      host     : dbConfig.host,
+      user     : dbConfig.user,
+      database : dbConfig.database,
+      password : dbConfig.password
+    });
+  }
+
+  connect(){
+    if(!isConnectionOpen){
+      connection.connect();
+      isConnectionOpen = true;
+    }
+  }
+
+  closeConnection(){
+    connection.end();
+  }
+
+  getSchemaInformation(tableName){
+    return new Promise((resolve, reject) => {
+      connection.query(`SHOW FULL FIELDS FROM ${tableName}`, function (error, results, fields) {
+        if (error) {
+          reject(error);
+        } else {
+          let retObj = {};
+          retObj[tableName] = results;
+          resolve(retObj);
+        }
+      });  
+    })
+  }
+}

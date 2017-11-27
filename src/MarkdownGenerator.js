693d0892808ea0fb6d1db2065c001a21ec94ed84
Kevin McIntosh <kevjames3@gmail.com>
2017-11-26 18:25:20 -0800

    Got the initial stuff working, it looks like it generates nicely

--- src/MarkdownGenerator.js
+++ src/MarkdownGenerator.js
@@ -1,4 +1,15 @@
-const fs                = require('fs');
+const fs         = require('fs');
+const tableOrder = [
+  "Field",
+  "Type",
+  "Collation",
+  "Null",
+  "Key",
+  "Default",
+  "Extra",
+  "Privileges",
+  "Comment"
+];
 
 function getMarkdownStub(filePath){
   let retValue = "";
@@ -14,10 +25,43 @@ function getMarkdownStub(filePath){
 function generateOutputForTable(name, data, tableConfig){
   let output = [];
   output.push(`# \`${name}\`\n`);
+  if(tableConfig.before){
+    output.push("\n");
+    output.push(getMarkdownStub(tableConfig.before));
+  }
+  output.push("\n\n");
+
+  //Let's do the columns
+  data.forEach((row, index) => {
+    if(index == 0) { //If it is the first row, let's put the header first
+      //Column header section
+      output.push("|");
+      tableOrder.forEach((column) => {
+        output.push(` ${column} |`);
+      });
+      output.push("\n");
+
+      //Row beneath the column header
+      output.push("|");
+      for(let i = 0; i < tableOrder.length; i++){
+        output.push(" --- |");
+      }
+      output.push("\n");
+    }
+
+    //Now time for the data
+    output.push("|"); 
+    tableOrder.forEach((column) => {
+      output.push(` ${row[column]} |`);
+    });
+    output.push("\n");
+  })
+
+  if(tableConfig.after){
+    output.push("\n");
+    output.push(getMarkdownStub(tableConfig.after));
+  }
   output.push("\n");
-  output.push(getMarkdownStub(tableConfig.before));
-  output.push("\n");
-  output.push(getMarkdownStub(tableConfig.after));
   return output.join("");
 }
 
@@ -29,6 +73,11 @@ module.exports = class MarkdownGenerator {
   }
 
   generateMarkdown(tables){
+    //Generate the header
+    this.output.push("<!--- ")
+    this.output.push("This output was generated automatically.  Take care when editing.")
+    this.output.push("-->")
+    this.output.push("\n");
     this.output.push(`# ${this.globalSettings.documentName}\n`);
     this.output.push("\n");
     this.output.push(getMarkdownStub(this.globalSettings.before));
